a2016d8792fd7c3a25e4dad28361d71af21dfbe5
leedasom <ds.lee15@cj.net>
2022-07-15 11:08:05 +0900

    1

---
+++ MemoSwiftUI/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  MemoSwiftUI
+//
+//  Created by 이다솜 on 2022/07/15.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
---
+++ MemoSwiftUI/Memo/Memo.swift
@@ -0,0 +1,21 @@
+//
+//  Memo.swift
+//  MemoSwiftUI
+//
+//  Created by 이다솜 on 2022/07/15.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
