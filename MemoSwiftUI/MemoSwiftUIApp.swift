914ed0f649bcf42cc51e27159986d32c6a66f664
leedasom <ds.lee15@cj.net>
2022-07-15 10:59:39 +0900

    Initial Commit

---
+++ MemoSwiftUI/MemoSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemoSwiftUIApp.swift
+//  MemoSwiftUI
+//
+//  Created by 이다솜 on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct MemoSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
